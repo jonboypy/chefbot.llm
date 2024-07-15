1248e3663f85237ae4d6486990342ba6ce2d38e3
Jonathan Hampton <ghostride.ai@gmail.com>
2024-07-15 03:39:19 +0000

    fix small bugs

--- data.py
+++ data.py
@@ -57,11 +57,12 @@ class DataModule(LightningDataModule):
         self.cfg = cfg
 
     def setup(self, stage: str) -> None:
-        if stage == 'fit':
+        if stage in ('fit', 'validate', 'test'):
             # (1) Get tokenizer instance
-            self.tokenizer = self.trainer.lightning_module.tokenizer
+            try: self.tokenizer = self.trainer.lightning_module.tokenizer
+            except: self.tokenizer = self.cfg.tokenizer.create_instance()
             # (2) Load dataset from JSON
-            with open(self.cfg.dataset) as f:
+            with open(self.cfg.data_src) as f:
                 data = json.load(f)
             # (3) Re-organize data structure
             data = {k1+'/'+k2: v2 for (k1, v1) in
@@ -80,30 +81,35 @@ class DataModule(LightningDataModule):
                     'response': sample['instructions']}
                 data_rorg += [sample1, sample2]
             # (4) Perform train/validation split of data
-            train_len = len(data_rorg) * self.cfg.train_size
+            train_len = int(len(data_rorg) * self.cfg.train_size)
             if train_len % 2: train_len += 1
             train_data = data_rorg[:train_len]
             val_data = data_rorg[train_len:]
             # (5) Instantiate training & validation datasets
-            self.train_ds = self.cfg.dataset.create_instance({'data': train_data})
-            self.val_ds = self.cfg.dataset.create_instance({'data': val_data})
+            self.train_ds = self.cfg.dataset.create_instance(
+                {'data': train_data, 'tokenizer': self.tokenizer})
+            self.val_ds = self.cfg.dataset.create_instance(
+                {'data': val_data, 'tokenizer': self.tokenizer})
         else:
             raise NotImplementedError
         
-    def collate_fn(self, samples: list[tuple]) -> tuple[Tensor]:
-        x = [sample[0] for sample in samples]
-        y = [sample[1] for sample in samples]
+    def _collate_fn(self, samples: list[tuple]) -> tuple[Tensor]:
+        x = [sample[0][0] for sample in samples]
+        y = [sample[1][0] for sample in samples]
         x = torch.nn.utils.rnn.pad_sequence(
             x, True, self.tokenizer.pad_token_id)
         y = torch.nn.utils.rnn.pad_sequence(y, True, -100)
         return x, y
         
     def train_dataloader(self) -> DataLoader:
-        return self.cfg.train_dataloader.create_instance({'dataset': self.train_ds})
+        return self.cfg.train_dataloader.create_instance(
+            {'dataset': self.train_ds, 'collate_fn': self._collate_fn})
 
     def val_dataloader(self) -> DataLoader:
-        return self.cfg.val_dataloader.create_instance({'dataset': self.val_ds})
+        return self.cfg.val_dataloader.create_instance(
+            {'dataset': self.val_ds, 'collate_fn': self._collate_fn})
 
     def test_dataloader(self) -> DataLoader:
-        return self.cfg.val_dataloader.create_instance({'dataset': self.val_ds})
-
+        return self.cfg.val_dataloader.create_instance(
+            {'dataset': self.val_ds, 'collate_fn': self._collate_fn})
+    
